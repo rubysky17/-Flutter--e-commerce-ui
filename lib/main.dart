fbd21d9988a78bc6ab73310ba967ce4cc62a8c38
Mạnh Đạt Võ <dat.vomanh@haravan.com>
2022-10-02 19:49:55 +0700

    feat: create config and constant started

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_ui/main/constant.dart';
+import 'package:e_commerce_ui/main/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,13 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'E-Commerce Appication',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: "Muli",
+        textTheme: TextTheme(
+          bodyText1: TextStyle(
+            color: kTextColor,
+          ),
+          bodyText2: TextStyle(
+            color: kTextColor,
+          ),
+        ),
       ),
-      home: const Scaffold(
-        body: Text("Hello world"),
-      ),
+      home: SplashScreen(),
     );
   }
 }
